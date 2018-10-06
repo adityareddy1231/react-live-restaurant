05ee3177c7eb3de3259151761521fe917fcdc08e
Aditya Reddy <adityareddy1231@gmail.com>
2018-10-05 21:16:59 -0600

    Added react router and setup URLs and basic store slection and store page

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { getFunName } from "../helpers";
+import PropTypes from "prop-types";
 
 class StorePicker extends React.Component {
-  render(){
-    return <p>Hello</p>
+  goToStore(event){
+    event.preventDefault();
+    //grabbing text from box
+    const storeId = this.storeInput.value;
+      console.log(storeId);
+      this.context.router.history.push("/store/${storeId}");
+
   }
+
+  render(){
+    return (
+      <form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
+        <button type="Submit">Visit Store</button>
+      </form>
+    )
+  }
+}
+
+StorePicker.contextTypes = {
+  router: PropTypes.object
 }
 
 export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import React from "react";
-import { render } from "react-dom";
+import {render} from "react-dom";
+import {BrowserRouter} from 'react-router-dom'
+import {Route, Switch} from "react-router";
+import "./css/style.css"
+import App from "./components/App";
 import StorePicker from "./components/StorePicker";
+import NotFound from "./components/NotFound";
 
-render(<StorePicker/>, document.querySelector("#main"));
+const Root = () => {
+  return (<BrowserRouter>
+    <div>
+      <Switch>
+        <Route exact path="/" component={StorePicker}/>
+        <Route path="/store/:storeId" component={App}/>
+        <Route component={NotFound}/>
+      </Switch>
+    </div>
+  </BrowserRouter>)
+}
+render(<Root/>, document.querySelector("#main"));
